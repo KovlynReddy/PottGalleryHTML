ecd56e39df4ce9d3f0f549c29135588fc58e9078
Kovlyn <kovlyn.reddy@gmail.com>
2021-07-13 13:56:30 +0200

    newImages

--- WeedGallaery.js
+++ WeedGallaery.js
@@ -3,58 +3,19 @@
 // var fs = require('fs');
 // var files = fs.readdirSync('/downloads');
 
-var images = ['Images/IMG-20210705-WA0043.jpg',
-            'Images/IMG-20210705-WA0044.jpg',
-            'Images/IMG-20210705-WA0045.jpg',
-            'Images/IMG-20210705-WA0048.jpg',
-            'Images/IMG-20210705-WA0050.jpg',
-            'Images/IMG-20210705-WA0051.jpg',
-            'Images/IMG-20210705-WA0054.jpg',
-            'Images/IMG-20210705-WA0056.jpg',
-            'Images/IMG-20210705-WA0060.jpg',
-            'Images/IMG-20210705-WA0061.jpg',
-            'Images/IMG-20210705-WA0064.jpg',
-            'Images/IMG-20210705-WA0066.jpg',
-            'Images/IMG-20210705-WA0067.jpg',
-            'Images/IMG-20210705-WA0068.jpg',
-            'Images/IMG-20210705-WA0069.jpg',
-            'Images/IMG-20210705-WA0070.jpg',
-            'Images/IMG-20210705-WA0071.jpg',
-            'Images/IMG-20210705-WA0072.jpg',
-            'Images/IMG-20210705-WA0073.jpg',
-            'Images/IMG-20210705-WA0074.jpg',
-            'Images/IMG-20210705-WA0076.jpg',
-            'Images/IMG-20210705-WA0079.jpg',
-            'Images/IMG-20210705-WA0080.jpg',
-            'Images/IMG-20210705-WA0082.jpg',
-            'Images/IMG-20210705-WA0083.jpg',
-            'Images/IMG-20210705-WA0084.jpg',
-            'Images/IMG-20210705-WA0085.jpg',
-            'Images/IMG-20210705-WA0087.jpg',
-            'Images/IMG-20210705-WA0089.jpg',
-            'Images/IMG-20210705-WA0090.jpg',
-            'Images/IMG-20210705-WA0093.jpg',
-            'Images/IMG-20210705-WA0094.jpg',
-            'Images/IMG-20210705-WA0096.jpg',
-            'Images/IMG-20210705-WA0097.jpg',
-            'Images/IMG-20210705-WA0098.jpg',
-            'Images/IMG-20210705-WA0099.jpg',
-            'Images/IMG-20210705-WA0101.jpg',
-            'Images/IMG-20210705-WA0102.jpg',
-            'Images/IMG-20210705-WA0103.jpg',
-            'Images/IMG-20210705-WA0104.jpg'];
+var images = ['IMG_20210709_164151_1.jpg', 'IMG_20210709_164152_1.jpg', 'IMG_20210709_164221.jpg', 'IMG_20210709_164221_2.jpg', 'IMG_20210709_164229_1.jpg', 'IMG_20210709_164249.jpg', 'IMG_20210709_164637.jpg', 'IMG_20210709_164854.jpg', 'IMG_20210709_164939.jpg', 'IMG_20210709_165002.jpg', 'IMG_20210709_165044.jpg', 'IMG_20210709_165401.jpg', 'IMG_20210709_165440_1.jpg', 'IMG_20210709_165600.jpg', 'IMG_20210709_165706_1.jpg', 'IMG_20210709_165708_1.jpg', 'IMG_20210709_165712_1.jpg', 'IMG_20210709_165807.jpg', 'IMG_20210709_165900.jpg', 'IMG_20210709_165902.jpg', 'IMG_20210709_165904_1.jpg', 'IMG_20210709_165906_1.jpg', 'IMG_20210709_165930_1.jpg', 'IMG_20210709_165935_1.jpg', 'IMG_20210709_165938_1.jpg', 'IMG_20210709_170035_1.jpg', 'IMG_20210709_170038_1.jpg', 'IMG_20210709_170051_1.jpg', 'IMG_20210709_170055_1.jpg', 'IMG_20210709_170100.jpg', 'IMG_20210709_170110.jpg', 'IMG_20210709_170325.jpg', 'IMG_20210709_170713_1.jpg', 'IMG_20210709_170720.jpg', 'IMG_20210709_170723_1.jpg', 'IMG_20210709_170748.jpg', 'IMG_20210709_170810_1.jpg', 'IMG_20210709_170838_1.jpg', 'IMG_20210709_170844_1.jpg', 'IMG_20210709_170846.jpg', 'IMG_20210709_171025.jpg', 'IMG_20210709_171029_1.jpg', 'IMG_20210709_171034_1.jpg', 'IMG_20210709_171039_1.jpg', 'IMG_20210709_171043_1.jpg', 'IMG_20210709_171053_1.jpg', 'IMG_20210709_171124_1.jpg', 'IMG_20210709_171126_1.jpg', 'IMG_20210709_171129_1.jpg', 'IMG_20210709_171135.jpg', 'IMG_20210709_171137_1.jpg', 'IMG_20210709_171141_1.jpg', 'IMG_20210709_171144_1.jpg', 'IMG_20210709_171337.jpg', 'IMG_20210709_171339.jpg'];
 
 function CarouselItem(image,index){
     if(index == 1 )
     {return `<div class="carousel-item active">
-    <img src="${image}" class="d-block h-50 mx-auto d-block image-fluid rounded" alt="...">
+    <img src="Images/${image}" class="d-block h-1 mx-auto d-block  rounded img-thumbnail" alt="...">
     <div class="carousel-caption d-none d-md-block">
       <h5>Weed</h5>
       <p>Home Grown</p>
     </div>
   </div>`;}
     var item = `<div class="carousel-item">
-    <img src="${image}" class="d-block h-50  mx-auto d-block image-fluid rounded" alt="...">
+    <img src="Images/${image}" class="d-block h-1 mx-auto d-block rounded img-thumbnail" alt="...">
     <div class="carousel-caption d-none d-md-block">
       <h5>Weed</h5>
       <p>Home Grown</p>
